f14c5aa6862e14688ea4a6f14d41209281153d5a
Spencer <spencervess@gmail.com>
2020-09-12 12:22:51 -0400

    initial commit

---
+++ src/commands/util.rs
@@ -0,0 +1,52 @@
+use serenity::framework::standard::{macros::command, Args, CommandResult};
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+use serenity::utils::parse_mention;
+
+#[command]
+#[only_in(guilds)]
+pub async fn add_role(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let member = parse_mention(args.single::<String>().unwrap()).unwrap();
+    let mut member = ctx.http.get_member(msg.guild_id.unwrap().0, member).await?;
+
+    let role = parse_mention(args.single::<String>().unwrap()).unwrap();
+
+    member.add_role(&ctx.http, RoleId(role)).await?;
+
+    msg.channel_id
+        .say(
+            &ctx.http,
+            format!(
+                "Gave role {} to {}",
+                RoleId(role).to_role_cached(&ctx.cache).await.unwrap().name,
+                msg.author.name
+            ),
+        )
+        .await?;
+
+    Ok(())
+}
+
+#[command]
+#[only_in(guilds)]
+pub async fn remove_role(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let member = parse_mention(args.single::<String>().unwrap()).unwrap();
+    let mut member = ctx.http.get_member(msg.guild_id.unwrap().0, member).await?;
+
+    let role = parse_mention(args.single::<String>().unwrap()).unwrap();
+
+    member.remove_role(&ctx.http, RoleId(role)).await?;
+
+    msg.channel_id
+        .say(
+            &ctx.http,
+            format!(
+                "Removed role {} from {}",
+                RoleId(role).to_role_cached(&ctx.cache).await.unwrap().name,
+                msg.author.name
+            ),
+        )
+        .await?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,90 @@
-fn main() {
-    println!("Hello, world!");
+mod commands;
+
+use log::{error, info};
+use serenity::{
+    async_trait,
+    client::bridge::gateway::ShardManager,
+    framework::{standard::macros::group, StandardFramework},
+    http::Http,
+    model::{
+        event::ResumedEvent,
+        gateway::{Activity, Ready},
+    },
+    prelude::*,
+};
+use std::{collections::HashSet, env, sync::Arc};
+use tokio::signal;
+
+use commands::{math::*, util::*};
+
+struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        info!("Connected as {}", ready.user.name);
+
+        ctx.set_activity(Activity::playing("name")).await;
+    }
+
+    async fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Resumed");
+    }
+}
+
+#[group]
+#[commands(multiply, add_role, remove_role)]
+struct General;
+
+#[tokio::main]
+async fn main() {
+    kankyo::load(false).expect("Failed to load .env file");
+
+    env_logger::init();
+
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token to be in the environment");
+
+    let http = Http::new_with_token(&token);
+
+    let (owners, _bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            owners.insert(info.owner.id);
+
+            (owners, info.id)
+        }
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.owners(owners).prefix("!"))
+        .group(&GENERAL_GROUP);
+
+    let mut client = Client::new(&token)
+        .framework(framework)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+    }
+
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        signal::ctrl_c().await.expect("Error");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+
+    if let Err(why) = client.start_autosharded().await {
+        error!("Client error: {:?}", why);
+    }
 }
