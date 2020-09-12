5e50ff5ea8a31064205c38c6cf8b537c42018989
Spencer <spencervess@gmail.com>
2020-09-12 15:57:35 -0400

    fix owner_check to not send message when running help command

--- src/util/checks.rs
+++ src/util/checks.rs
@@ -4,22 +4,10 @@ use serenity::prelude::*;
 
 #[check]
 #[name = "owner"]
-async fn owner_check(
-    ctx: &Context,
-    msg: &Message,
-    _: &mut Args,
-    _: &CommandOptions,
-) -> CheckResult {
+async fn owner_check(_: &Context, msg: &Message, _: &mut Args, _: &CommandOptions) -> CheckResult {
     if msg.author.id != 253290704384557057 {
-        if let Err(_) = msg
-            .channel_id
-            .say(&ctx.http, "You lack admin permissions")
-            .await
-        {
-            return CheckResult::new_unknown();
-        }
         return CheckResult::new_user("You lack admin permissions");
-    };
+    }
 
     CheckResult::Success
 }
