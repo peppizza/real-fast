8526b3a3a13b96c330e27aee97c8c7a50fa7f703
Spencer <spencervess@gmail.com>
2020-11-13 20:02:03 -0500

    remove allow attribute

--- src/commands/music/consts.rs
+++ src/commands/music/consts.rs
@@ -1,6 +1,3 @@
-// for some reason this is dead code and idk why
-#[allow(dead_code)]
 pub const SONGBIRD_EXPECT: &str = "Songbird Voice client placed in at initialisation.";
 
-#[allow(dead_code)]
 pub const VOICEQUEUEMANAGER_NOT_FOUND: &str = "Expected VoiceQueueManager in ShareMap.";
